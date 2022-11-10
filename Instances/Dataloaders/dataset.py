ee3930ed8e614750c022c36f57165c28974229a8
JunHyung1206 <pjh120610@naver.com>
2022-11-10 13:15:54 +0000

    데이터 셋 수정 및 각각의 원소 들어갈 수 있게 구현 완료

--- Instances/Dataloaders/dataset.py
+++ Instances/Dataloaders/dataset.py
@@ -13,14 +13,18 @@ class Dataset(torch.utils.data.Dataset):
     # 학습 및 추론 과정에서 데이터를 1개씩 꺼내오는 곳
     def __getitem__(self, idx):
         input_ids, attention_mask, token_type_ids = (
-            self.inputs["input_ids"][idx],
-            self.inputs["attention_mask"][idx],
-            self.inputs["token_type_ids"][idx],
+            self.inputs[idx]["input_ids"],
+            self.inputs[idx]["attention_mask"],
+            self.inputs[idx]["token_type_ids"],
         )
         if len(self.targets) == 0:  # 정답이 있다면 else문을, 없다면 if문을 수행합니다
-            return (input_ids, attention_mask, token_type_ids)
+            return (
+                torch.tensor(input_ids),
+                torch.tensor(attention_mask),
+                torch.tensor(token_type_ids),
+            )
         else:
-            return input_ids, attention_mask, token_type_ids, torch.tensor(self.targets[idx])
+            return torch.tensor(input_ids), torch.tensor(attention_mask), torch.tensor(token_type_ids), torch.tensor(self.targets[idx])
 
     def __len__(self):
         return len(self.inputs)  # 입력하는 개수만큼 데이터를 사용합니다
