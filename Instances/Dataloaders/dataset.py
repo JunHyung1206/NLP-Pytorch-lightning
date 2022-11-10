b37a9abf63baeedc4a6921f8cc6b00b9ae9a06ee
JunHyung1206 <pjh120610@naver.com>
2022-11-10 12:59:23 +0000

    데이터 셋 수정

--- Instances/Dataloaders/dataset.py
+++ Instances/Dataloaders/dataset.py
@@ -12,11 +12,15 @@ class Dataset(torch.utils.data.Dataset):
 
     # 학습 및 추론 과정에서 데이터를 1개씩 꺼내오는 곳
     def __getitem__(self, idx):
-
+        input_ids, attention_mask, token_type_ids = (
+            self.input_data["input_ids"][idx],
+            self.input_data["attention_mask"][idx],
+            self.input_data["token_type_ids"][idx],
+        )
         if len(self.targets) == 0:  # 정답이 있다면 else문을, 없다면 if문을 수행합니다
-            return torch.tensor(self.inputs[idx])
+            return (input_ids, attention_mask, token_type_ids)
         else:
-            return torch.tensor(self.inputs[idx]), torch.tensor(self.targets[idx])
+            return input_ids, attention_mask, token_type_ids, torch.tensor(self.targets[idx])
 
     def __len__(self):
         return len(self.inputs)  # 입력하는 개수만큼 데이터를 사용합니다
