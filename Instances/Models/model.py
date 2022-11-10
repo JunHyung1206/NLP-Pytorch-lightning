c0f2913a36ebfaac49aa14899811499d6e11aac8
JunHyung1206 <pjh120610@naver.com>
2022-11-10 13:04:07 +0000

    모델 수정

--- Instances/Models/model.py
+++ Instances/Models/model.py
@@ -27,36 +27,36 @@ class Model(pl.LightningModule):
         if self.use_freeze:
             self.freeze()
 
-    def forward(self, x):
-        x = self.plm(x)["logits"]
+    def forward(self, input_ids, attention_mask, token_type_ids, labels):
+        x = self.plm(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, labels=labels)["logits"]
         return x
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.loss_func(logits, y.float())
+        input_ids, attention_mask, token_type_ids, labels = batch
+        logits = self(input_ids, attention_mask, token_type_ids, labels)
+        loss = self.loss_func(logits, labels.float())
         self.log("train_loss", loss)
 
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.loss_func(logits, y.float())
+        input_ids, attention_mask, token_type_ids, labels = batch
+        logits = self(input_ids, attention_mask, token_type_ids, labels)
+        loss = self.loss_func(logits, labels.float())
         self.log("val_loss", loss)
-        self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
+        self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), labels.squeeze()))
 
         return loss
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
+        input_ids, attention_mask, token_type_ids, labels = batch
+        logits = self(input_ids, attention_mask, token_type_ids, labels)
 
-        self.log("test_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
+        self.log("test_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), labels.squeeze()))
 
     def predict_step(self, batch, batch_idx):
-        x = batch
-        logits = self(x)
+        input_ids, attention_mask, token_type_ids, labels = batch
+        logits = self(input_ids, attention_mask, token_type_ids, labels)
 
         return logits.squeeze()
 
